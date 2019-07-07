fbd527471eabe66638b8e34b74991880c67a80ca
josharsh <500060668@stu.upes.ac.in>
2019-07-07 14:25:09 +0530

    Updates

---
+++ precache-manifest.41216bb5806681f1ad837be0ab916001.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9eb02874f84fe465cea7354ede8832ca",
+    "url": "/contactmanager/index.html"
+  },
+  {
+    "revision": "d92a379b016f705d0542",
+    "url": "/contactmanager/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "bb484f65690876fd1deb",
+    "url": "/contactmanager/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "d92a379b016f705d0542",
+    "url": "/contactmanager/static/js/2.7f38287d.chunk.js"
+  },
+  {
+    "revision": "bb484f65690876fd1deb",
+    "url": "/contactmanager/static/js/main.f3cd464b.chunk.js"
+  },
+  {
+    "revision": "a9b80a0b0e722e0d3664",
+    "url": "/contactmanager/static/js/runtime~main.884ecd02.js"
+  }
+]);
